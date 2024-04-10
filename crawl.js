71d8b12c31dd6be14af01fd2fb892f2d7a5c3065
Aneesh Bakshi <aneesh.bakshi1@gmail.com>
2024-04-10 16:54:52 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  return urls;
+}
+
 function normalizaURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -7,4 +35,4 @@ function normalizaURL(urlString) {
   return hostPath;
 }
 
-module.exports = { normalizaURL };
+module.exports = { normalizaURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizaURL } = require("./crawl.js");
+const { normalizaURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizaURL strip protocol and trailing slashes", () => {
@@ -12,5 +12,65 @@ test("normalizaURL convert to lowercase", () => {
   const input = "https://www.Blog.boot.dev/";
   const actual = normalizaURL(input);
   const expected = "www.blog.boot.dev";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+        <a href="https://www.blog.boot.dev/">Boot.dev Blog</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://www.blog.boot.dev/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://www.blog.boot.dev/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+        <a href="/path/">Boot.dev Blog</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://www.blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://www.blog.boot.dev/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute and relative", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+        <a href="https://www.blog.boot.dev/path1">Boot.dev Blog Path 1</a>
+        <a href="/path2">Boot.dev Blog Path 2</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://www.blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://www.blog.boot.dev/path1",
+    "https://www.blog.boot.dev/path2",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid urls", () => {
+  const inputHTMLBody = `
+    <html>
+      <body>
+          <a href="invalid">invalid</a>
+      </body>
+    </html>
+    `;
+  const inputBaseURL = "https://www.blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
